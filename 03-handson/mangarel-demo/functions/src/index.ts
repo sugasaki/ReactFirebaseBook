564f29c5bde7fad2f2408388e757a186de99ca21
sugasaki <sugasaki@gmail.com>
2021-01-04 14:48:13 +0900

    ３章の１まで完了

--- 03-handson/mangarel-demo/functions/src/index.ts
+++ 03-handson/mangarel-demo/functions/src/index.ts
@@ -1,8 +1,21 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
+import { collectionName } from './services/mangarel/constants';
+
+admin.initializeApp();
+
 export const helloWorld = functions.https.onRequest((request, response) => {
   response.send('Hello from Firebase!');
 });
+
+export const publishers = functions
+  .region('asia-northeast1')
+  .https.onRequest(async (req, res) => {
+    const snap = await admin
+      .firestore()
+      .collection(collectionName.publishers)
+      .get();
+    const data = snap.docs.map((doc) => doc.data());
+    res.send({ data });
+  });
